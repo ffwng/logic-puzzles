3ba5657eaa9d151ab658ec177ef45c91a72ccc35
Richard Pilz <richard.pilz.online@gmx.de>
2014-11-14 23:23:20 +0100

    Fixes examples

--- Examples/Hammer.hs
+++ Examples/Hammer.hs
@@ -4,8 +4,11 @@ module Main where
 -- http://www.onlinewahn.de/hammer-r.htm
 -- 0.2% of population… LOL
 
-import Puzzle2
-import QQ
+import Prelude hiding ((&&), not)
+
+import Puzzle
+import Puzzle.QQ
+import Ersatz.Bit
 
 [puzzle|
   Farbe = Rot | Silber | Blau | Braun | Gruen
@@ -19,23 +22,21 @@ import QQ
 neben :: Pos -> Pos -> Bool
 neben (P i) (P j) = abs (i-j) == 1
 
-is' a b = is (==a) b
-
-test :: Test '[Pos, Farbe, Auto, Beruf, Stadt, Musik] IO ()
-test = do
-  is' Rot Ferrari
-  is' Silber Lehrer
-  is' VW Madonna
-  is' BMW Muenchen
-  is2 neben BMW Blau
-  is2 neben Hamburg Braun
-  is' Abba Metzger
-  is2 neben Beatles Lehrer
-  is' Notar Koeln
-  is2 neben Blau Smart
-  is' Ford Schreiner
-  is' Gruen Hamburg
-  is2 neben Berlin Baecker
-  is' (P 4) Eminem
-  is2 (\a b -> not $ neben a b) Stuttgart BMW
+test :: Test '[Pos, Farbe, Auto, Beruf, Stadt, Musik]
+test =
+  is Rot Ferrari &&
+  is Silber Lehrer &&
+  is VW Madonna &&
+  is BMW Muenchen &&
+  is2 neben BMW Blau &&
+  is2 neben Hamburg Braun &&
+  is Abba Metzger &&
+  is2 neben Beatles Lehrer &&
+  is Notar Koeln &&
+  is2 neben Blau Smart &&
+  is Ford Schreiner &&
+  is Gruen Hamburg &&
+  is2 neben Berlin Baecker &&
+  is (P 4) Eminem &&
+  not (is2 neben Stuttgart BMW)
   
